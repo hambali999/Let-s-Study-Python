012570610dfc7c48da457810617e0dba482137c1
hambali999 <chorongbali@gmail.com>
2021-07-05 20:22:11 +0800

    part2 inheritance

--- oop/inheritance/part2.py
+++ oop/inheritance/part2.py
@@ -23,20 +23,25 @@ class Student():
 
 class SoftwareEngineer(Student):
     def __init__(self, name, age, major):
-        super().__init__(name, age)              #DONT PUT SELF
+        super().__init__(name, age) #DONT PUT SELF
         self.major = major
 
 class Designer(Student):
     def __init__(self, name, age, major):
-        super().__init__(name, age)             #DONT PUT SELF
+        super().__init__(name, age) #DONT PUT SELF
         self.major = major
 
 
 # Initializing the objects
-student1 = SoftwareEngineer("Cindy", 24, 'SE')
-student2 = Student("Alice", 23)
-student3 = Student("Elon", 40)
+student1 = SoftwareEngineer("Cindy", 24, 'Programming')
+student2 = Designer("Alice", 23, "Design")
+student3 = Designer("Elon", 40, "Art")
 
 # Print
-print(student1.major)
+# print(student1.major)
+
+print(f"Hi my name is {student1.name} and my age is {student1.age}, I majored in {student1.major}")
+print(f"Hi my name is {student2.name} and my age is {student2.age}, I majored in {student2.major}")
+print(f"Hi my name is {student3.name} and my age is {student3.age}, I majored in {student3.major}")
+
 
