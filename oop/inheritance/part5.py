62f8848db1fbb41d279d51e90c75a6b48195618a
hambali999 <chorongbali@gmail.com>
2021-07-06 01:39:28 +0800

    level

---
+++ oop/inheritance/part5.py
@@ -0,0 +1,31 @@
+print("START PROGRAM ... ")
+print("----------------")
+
+class Student():
+    def __init__(self, name, age, school, role):
+        self.name = name
+        self.age = age
+        self.school = school
+        self.role = role
+
+class SoftwareEngineer(Student): #PLEASE REMEMBER TO TAKE IN THE PARENT CLASS 'Student' INSIDE THIS CLASS FFS
+    def __init__(self, name, age, school, role, level):
+        super().__init__(name, age, school, role)
+        self.role = role
+        self.level = level
+
+class Artist(Student):
+    def __init__(self, name, age, school, role, level):
+        super().__init__(name, age, school, role)
+        self.role = role
+        self.level = level
+
+student1 = SoftwareEngineer("John", 21, "Computer School", 'Software Engineer', 2)
+student2 = Artist("Jake", 25, "Art School", 'Artist', 3)
+student3 = Student("Alice", 25, "Art School", "Student")
+
+print(f"Whats up peeps, my name is {student1.name} and i am {student1.age}. I am currently attending {student1.school} as a {student1.role}")
+        
+print(f"Whats up peeps, my name is {student2.name} and i am {student2.age}. I am currently attending {student2.school} as a {student2.role}")
+
+print(f"Whats up peeps, my name is {student3.name} and i am {student3.age}. I am currently attending {student3.school} as a {student3.role}")
